452d874fba7b9bd830bf2de5304183cada245191
Hugo Pérez <47252665+Hugondon@users.noreply.github.com>
2020-12-07 00:58:47 -0600

    Update Wave_LUT.m

--- MATLAB_Tests/Modulations/Wave_LUT.m
+++ MATLAB_Tests/Modulations/Wave_LUT.m
@@ -1,10 +1,10 @@
 clear; clc;   % Clear The Previous Points
-Ns     = 100; % Set The Number of Sample Points
+Ns     = 128; % Set The Number of Sample Points
 RES    = 12;  % Set The DAC Resolution
 OFFSET = 0;   % Set An Offset Value For The DAC Output
 %------------[ Calculate The Sample Points ]-------------
 T = 0:((2*pi/(Ns-1))):(2*pi);
-Y = sawtooth(T, 0.5); % Triangular
+Y = sin(T);
 Y = Y + 1;    
 Y = Y*((2^RES-1)-2*OFFSET)/(2+OFFSET); 
 Y = round(Y);                  
@@ -15,3 +15,5 @@ fprintf('%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, \n', Y);
 %-------------------[ Other Examples To Try ]-------------------
 %  Y = diric(T, 13);     % Periodic Sinc
 %  Y = sawtooth(T)       % Sawtooth
+%  Y = sawtooth(T, 0.5); % Triangular
+%  Y = square(T);        % Square
